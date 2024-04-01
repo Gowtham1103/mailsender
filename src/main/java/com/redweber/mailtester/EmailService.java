c9310f6d6403c1962bf94ae71e24270518634b05
Gowtham1103 <gowtham19.bm@gmail.com>
2024-04-02 01:33:44 +0530

    upd

--- src/main/java/com/redweber/mailtester/EmailService.java
+++ src/main/java/com/redweber/mailtester/EmailService.java
@@ -1,6 +1,9 @@
 package com.redweber.mailtester;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,12 +13,27 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
-    public void sendSubscriptionEmail(String toEmail) {
+    public ResponseEntity<String> sendSubscriptionEmail(String toEmail) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(toEmail);
         message.setSubject("Thank You for Subscribing");
-        message.setText("Thank you for subscribing to our updates and exclusive deals!");
+        message.setText("" +
+                "Dear Customer,\n" +
+                "\n" +
+                "Thank you for subscribing to our updates and exclusive deals! We're thrilled to have you join our community.\n" +
+                "\n" +
+                "Stay tuned for exciting news, special offers, and insider information delivered straight to your inbox.\n" +
+                "\n" +
+                "If you have any questions or feedback, feel free to reach out to us anytime.\n" +
+                "\n" +
+                "Best regards,\n" +
+                "Thankyou !");
 
-        emailSender.send(message);
+        try {
+            emailSender.send(message);
+            return ResponseEntity.status(HttpStatus.OK).body("Email sent successfully.");
+        } catch (MailException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send email.");
+        }
     }
 }
--- src/main/java/com/redweber/mailtester/SubscriptionController.java
+++ src/main/java/com/redweber/mailtester/SubscriptionController.java
@@ -1,17 +1,22 @@
 package com.redweber.mailtester;
 
+import com.redweber.mailtester.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SubscriptionController {
+
     @Autowired
     private EmailService emailService;
 
     @PostMapping("/subscribe")
-    public void subscribe(@RequestBody String email) {
-        emailService.sendSubscriptionEmail(email);
+    public ResponseEntity<String> subscribe(@RequestBody String email) {
+        ResponseEntity<String> response = emailService.sendSubscriptionEmail(email);
+        return response;
     }
 }
