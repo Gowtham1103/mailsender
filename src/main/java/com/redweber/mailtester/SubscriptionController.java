980f802661c8924b752d8e07d307ae29ebaf5f2f
Gowtham1103 <gowtham19.bm@gmail.com>
2024-04-02 14:18:37 +0530

    nn

--- src/main/java/com/redweber/mailtester/SubscriptionController.java
+++ src/main/java/com/redweber/mailtester/SubscriptionController.java
@@ -4,11 +4,13 @@ import com.redweber.mailtester.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class SubscriptionController {
 
     @Autowired
